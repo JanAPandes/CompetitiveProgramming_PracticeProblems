4dc559762ee470815eeadc740f4151eefbaa1c99
JanAPandes <janalbert.pandes@smc.pshs.edu.ph>
2025-11-05 14:14:42 +0800

    Practice Problem #1
    This C++ code shall calculate the sum from 1 to the user's chosen number. Originally, this was only up to 5, but this has been modified to accomplish the challenge tasked.
    
    The code will ask for a number, and store it in the variable num. Another variables named sum shall get the sum from one to the specific number. A for loop is used here, the code can be seen down below:
    
    for (int i = 1; i <= num; i++){
            sum += i;
        }
    
    Once these are accomplished, the sum shall be printed.

---
+++ PracticeProblem1.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+int main(){
+    int num;
+    std::cout << "Enter a number:";
+    std::cin >> num;
+    
+    int sum = 0;
+    for (int i = 1; i <= num; i++){
+        sum += i;
+    }
+    
+    std::cout << "The sum from 1 to " << num << " is " << sum;
+    return 0;
+}
